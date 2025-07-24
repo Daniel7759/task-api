71bb451d58fec2bdb33aa4075e6b22bce1d972a7
Daniel7759 <danieljh7759@gmail.com>
2025-07-24 18:35:23 -0500

    Add CorsFilter for local frontend

--- src/main/java/com/example/retotecnico/security/SecurityConfig.java
+++ src/main/java/com/example/retotecnico/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @AllArgsConstructor
@@ -29,6 +36,7 @@ public class SecurityConfig{
 
 
         return http
+                .cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeHttpRequests(authz ->{
                     authz.requestMatchers("/users").permitAll();
@@ -55,5 +63,19 @@ public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+                Arrays.asList("http://localhost:4200", "https://your-frontend-domain.com") // Change this to your frontend URL
+        ); // Change this to your frontend URL
+        config.setAllowedHeaders(List.of("*")); //not recommended for production
+        config.setAllowedMethods(List.of("*")); //not recommended for production
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
